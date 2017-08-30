b39c650274b416827703a1b37d6778412e6f1f0c
sereepap <sereepap2029@gmail.com>
2017-08-31 02:53:39 +0700

    finish questions

---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="./favicon.ico">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/carousel.css?v=<?=date("YmdHis")?>" rel="stylesheet">
+    <link href="./css/stylesheet.css?v=<?=date("YmdHis")?>" rel="stylesheet">
+    <script src="./assets/js/jquery-3.2.1.js"></script>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark ">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="./img/logo_head.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="dog-cat-ico">
+            <li class="nav-item-img">
+              <a href="#"><img src="./img/cat_ico.png"></a>
+            </li>
+            <li class="nav-item-img">
+              <a href="#"><img src="./img/dog_ico.png"></a>
+            </li>
+            
+          </ul>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">หน้าแรก</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">เกี่ยวกับเรา</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Benefits">ประโยชน์ของ VetzPetz</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#exp">ประสบการณ์จากผู้ใช้</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">วิธีการผลิต</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">กิจกรรมของเรา</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">คำถามที่พบบ่อย</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">หาซื้อได้ที่ไหน</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ติดต่อเรา</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ questions.php
@@ -0,0 +1,477 @@
+<?php include ('header.php'); ?>
+    
+
+
+
+  <div class="container-fluid marketing first-container-margin">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <h2 class="questions-heading">Any Questions : <span class="text-in">คำถามที่พบบ่อย</span></h2>
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-1" href="javascript:;">สารอาหารจากอาหารสุนัขยังมีคุณค่าทางสารอาหารไม่เพียงพอต่อสุนัขอีกหรือ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-1" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>โดยมากแล้วอาหารสำหรับสุนัขมักจะทำมาจากส่วนผสมที่ไม่ค่อยจะมีคุณภาพ ส่วนมากมักจะผลิตจากเนื้อที่เหลือจากการบริโภค
+ของมนุษย์หรือไม่ก็เป็นเนื้อในส่วนที่มนุษย์ไม่นิยมจะบริโภค แม้กระทั่งผลิตภัณฑ์ที่เรียกตนเองว่าจัดอยู่ในกลุ่ม “อาหารจาก
+ธรรมชาติ” หรือ “พรีเมี่ยม” ก็ยังคงมีข้อสงสัยเกี่ยวกับคุณภาพของอาหารอยู่ดี ในอาหารสุนัขนั้นก็ยังมีความไม่สมดุลของ
+โอเมก้า 3 และ โอเมก้า 6 ซึ่งมีผลต่อความสมดุลในตัวสัตว์เลี้ยงของท่าน</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-2" href="javascript:;">สุนัขของฉันกินอาหารที่ปลอดสารหรือที่เรียกว่าออร์แกนิกอยู่ มันยังไม่ดีพออีกหรือ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-2" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>อย่าหลงกลพวกผลิตภัณฑ์ที่จำอ้างว่าเป็นพวก “ออร์แกนิก” หรือ “บริสุทธิ์” คุณมาถูกทางแล้วล่ะที่เลือกอาหารจำพวกปลอด
+สารเคมี แต่อย่างไรก็ตามสุนัขของท่านยังต้องการกรดไขมัน ซึ่งหาไม่ได้ในอาหารสุนัขทั่วไป มีเพียงแต่ VetzPetz® เท่านั้น
+ที่มีส่วนประกอบอันอุดมไปด้วยกรดไขมันที่จำเป็นถึง 200 เท่า มากกว่าที่หาได้จากธรรมชาติ หรือผลิตภัณฑ์จากออร์แกนิก</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-3" href="javascript:;">ลูกสุนัขของฉันสามารถใช้ผลิตภัณฑ์ของ VetzPetz® ได้หรือไม่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-3" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>แน่นอนว่าได้ มันเป็นวิธีที่ดีที่สุดเพื่อป้องกันไม่ให้เกิดปัญหาสุขภาพ สิ่งที่คุณต้องทำไปพร้อมกันนั่นก็คีอการจัดการให้สุนัข
+กินอย่างถูกสุขลักษณะทางโภชนาการ VetzPetz® สามารถช่วยในเรื่องของข้อต่อ รวมไปถึงช่วยดูแลในส่วนของหัวใจอีกด้วย</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-4" href="javascript:;">สุนัขของฉันกินกลูโคซามีน (Glucosamine) แล้วยังจำเป็นที่จะต้องใช้ VetzPetz® อีกหรือ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-4" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>กลูโคซามีน (Glucosamine) เป็นอาหารเสริมที่ดีสำหรับข้อต่อส่วนต่างๆของสุนัข VetzPetz® มีประโยชน์ที่หลากหลาย 
+อุดมไปด้วยกรดไขมันที่จำเป็นต่อสุนัข ช่วยในเรื่องของระบบภูมิคุ้มกัน ปัญหาด้านผิวและขนที่หลุดร่วง ปัญหาโรคหัวใจและ
+หลอดเลือด ซึ่งสุนัขไม่สามารถได้รับกรดไขมันที่จำเป็นเหล่านี้ได้อย่างเพียงพอจาก อาหารทั่วๆไปที่พวกเขากิน</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-5" href="javascript:;">ต้องใช้เวลานานเท่าไหร่จึงจะเริ่มความเปลี่ยนแปลง</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-5" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>สุนัขจำนวนมากมักจะเห็นผลในระยะเวลาเพียงแค่ไม่กี่วันหลังจากเริ่มใช้ VetzPetz® อย่างไรก็ตามเพื่อให้ผลที่เต็มที่ควรจะให้
+เวลาสักนิดรอดูผลหลังจากที่ใช้อย่างต่อเนื่องเป็นเวลาสัก 4-6 สัปดาห์ ในบางกรณีในระยะเริ่มแรกของการบำบัด ท่านควรจะให้ 
+VetzPetz® ในปริมาณ 2 เท่าของ ในช่วงระยะสองสัปดาห์แรก ซึ่งจะเป็นการเตรียมตัวเริ่มต้นกระบวนการบำบัดได้อย่างเต็มที่</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-6" href="javascript:;">สุนัขของฉันควรจะกิน VetzPetz® ในปริมาณเท่าไหร่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-6" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>กระบวนการผลิตของ VetzPetz® นั้นเป็นกระบวนการที่ใช้ระยะเวลานานเลยทีเดียว เราจะต้องเลี้ยงหอยแมลงภู่และรอเวลา
+ในการเก็บในช่วงที่ดีที่สุด โดยประมาณแล้วเราใช้เวลาในจุดนี้ถึง 15 - 18 เดือน ซึ่งจะเห็นว่า กว่าจะได้ผลิตภัณฑ์แต่ละขวด
+ต้องใช้ระยะเวลานานกว่าหนึ่งปี
+<br>
+<br>
+ท่านควรสอบถาม กับผู้ให้คำปรึกษาและดูแลสุนัขของท่านเกี่ยวกับปริมาณการใช้ โดยทั่วไปแล้ว 1-2 แคปซูลต่อวัน ก็เพียงพอ
+สำหรับสุนัขขนาดเล็กไปจนถึงสุนัขขนาดกลาง และใช้ปริมาณ 2-4 แคปซูลต่อวันสำหรับสุนัขที่มีขนาดใหญ่
+<br><br>
+ในบางกรณีในระยะเริ่มแรกของการบำบัด ท่านควรจะให้ VetzPetz® ในปริมาณ 2 เท่าของ ในช่วงระยะสองสัปดาห์แรก 
+ซึ่งจะเป็นการเตรียมตัวเริ่มต้นกระบวนการบำบัดได้อย่างเต็มที่</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-7" href="javascript:;">ต้องใช้เวลานานเท่าไหร่จึงจะเริ่มเห็นผล</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-7" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>สุนัขแต่ละตัวจะมีการตอบสนองที่ต่างกัน โดยทั่วไปจะสามารถเห็นได้หลังจาก 5 - 10 วันก็จะเริ่มมีการเปลี่ยนแปลงแล้ว 
+หลังจากนั้นอีกสัก 2 - 3 สัปดาห์ก็จะเริ่มเห็นได้ชัดเจน และจะดียิ่งขี้นเรื่อยๆหลงจากใช้ติดต่อกันไปเรื่อยๆ ทั้งนี้เพราะสุนัขของคุณ
+ได้รับกรดไขมันที่จำเป็นทั้ง 30 ชนิดจาก VetzPetz®</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-8" href="javascript:;">VetzPetz® ก่อให้เกิดผลข้างเคียงหรือไม่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-8" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>VetzPetz® ไม่ได้ก่อให้เกิดผลข้างเคียงใดๆ ซึ่งจุดนี้จะเห็นได้ว่าต่างกับการใช้ยา</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-9" href="javascript:;">เราจะทราบได้อย่างไรว่า เราควรให้อาหารเสริมในกลุ่มกรดไขมันโอเมก้า 3 แก่สุนัขของเราหรือไม่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-9" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>กรดไขมันของ VetzPetz® ทั้ง 30 ชนิด ล้วนเป็นประโยชน์ต่อสุนัขทุกช่วงอายุ ทุกขนาด และสายพันธุ์ เพื่อช่วยพวกเขา
+ในการป้องกันโรคที่เกี่ยวกับหัวใจ ช่วยลดไขมันในเลือด และช่วยทำให้อวัยวะทุกส่วนในร่างกายทำงานได้ดีขึ้น รวมไปถึงทั้ง
+ข้อต่อ ผิวหนังและเส้นขน</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-10" href="javascript:;">สามารถให้สุนัขกิน VetzPetz® ร่วมกับการใช้ยาอื่นๆได้หรือไม่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-10" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>ได้ , VetzPetz® เป็นอาหารเสริมเพื่อสุขภาพ ถ้าท่านมีข้อสงสัยอะไรท่านควรจะปรึกษาสัตวแพทย์ของท่าน</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-11" href="javascript:;">จะต้องเช็คกับสัตวแพทย์ก่อนหรือไม่</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-11" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>VetzPetz® นั้นเป็นสารสกัดบริสุทธิ์จากแหล่งธรรมชาติ ประกอบด้วยกรดไขมันอันสำคัญถึง 30 ชนิด ซึ่งสกัดจากหอยแมลงภู่
+เปลือกเขียว อย่างไรก็ตามท่านควรเช็คกับสัตวแพทย์ของท่านก่อนที่จะเริ่มใช้ผลิตภัณฑ์ ทั้งนี้ท่านอาจจะให้สัตวแพทย์ของท่าน
+อ่านรายงานผลทางวิทยาศาสตร์บนเว็บไซต์นี้ด้วยก็ได้</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-12" href="javascript:;">VetzPetz® มีแหล่งที่มาอย่างไร</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-12" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>VetzPetz® Antinol ประกอบไปด้วยส่วนผสมของไขมัน 5 ประเภท, ส่วนผสมจากทะเล 10 ชนิด และกรดไขมันอันเป็นประโยชน์
+กว่า 30 ประเภท อีกทั้งยังมีส่วนผสมของกรดไขมันโอเมก้า 3, ETA, OTA,DHA และ EPA ซึ่งสกัดมาจากหอยแมลงภู่
+เปลือกเขียวนิวซีแลนด์ น้ำมันที่สกัดได้จากหอยแมลงภู่นิวซีแลนด์นี้ได้รับการจดสิทธิบัตร ในชื่อว่า PCSO-524® เป็นสูตร
+เฉพาะอันเป็นเอกลักษณ์ที่ไม่มีอยู่ในอาหารเสริมสำหรับสุนัขชนิดอื่นๆ<br><br>
+
+หอยแมลงภู่เหล่านี้ได้รับการเลี้ยงและดูแลโดยบริษัท Marlborough Sounds ซึ่งตั้งอยู่ทางตอนเหนือของเกาะนิวซีแลนด์ใต้ 
+ในกำกับและควบคุมคุณภาพโดยรัฐบาลของประเทศนิวซีแลนด์ เพื่อให้มั่นใจว่าหอยแมลงภู่เหล่านี้ได้ถูกเลี้ยงในเขตของ 
+แหล่งน้ำบริสุทธิ์ของมหาสมุทรแปซิฟิก</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-13" href="javascript:;">ถ้าสุนัขไม่ยอมกลืนแคปซูลจะทำอย่างไร</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-13" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>ง่ายมาก เพียงแต่ท่านตัดแคปซูลแล้วบีบเอาน้ำมันออกมา ท่านสามารถใช้น้ำมันนี้โรยลงบนอาหารสุนัข หรือผสมกัยเนยถั่ว ชีส
+เพียงเท่านี้สุนัขก็สามารถกินได้ตามปกติ ท่านก็หมดกังวลได้</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-14" href="javascript:;">เกิดอะไรขึ้นถ้าผลิตภัณฑ์เกิดหมดอายุ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-14" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>หลังจากที่ผลิตภัณฑ์หมดอายุ โอเมก้า 3 จะเริ่มสลายตัว ทั้งนี้ถ้าท่านเก็บผลิตภัณฑ์ตามที่เราแนะนำ คือ หลีกเลี่ยงแสงแดด
+และความชื้น ผลิตภัณฑ์จะมีอายุถึง 2 ปี</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-15" href="javascript:;">ควรจะเก็บผลิตภัณฑ์ที่อุณหภูมิระดับใด</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-15" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>แนะนำให้เก็บ VetzPetz® ที่อุณหภูมิทั่วไปของห้อง พยายามเก็บให้ห่างจากแสงแดดและความชื้น</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-16" href="javascript:;">สุนัขจะได้รับประโยชน์อะไรจากกรดไขมันโอเมก้า 3</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-16" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>กรดไขมันโอเมก้า 3 และ โอเมก้า 6 เป็นไขมันพื้นฐานที่จำเป็นต่อสุขภาพของสุนัขทั้งในด้านของการเสริมสร้างสุขภาพ VetzPetz® 
+ได้มีส่วนผสมของกรดไขมันที่สำคัญถึง 30 ชนิด มากกว่าอาหารเสริมสำหรับสัตว์เลี้ยงทั่วไปที่ วางขายอยู่ในปัจจุบัน<br><br>
+
+กรดไขมันที่สุนัขได้รับนี้ จะถูกเปลี่ยนเป็นสารที่ชื่อว่า “Eicosanoids” ซึ่งสารชนิดนี้มีผลในการต่อต้านและบรรเทาอาการอักเสบ
+ตามอวัยวะต่างๆของสัตว์เลี้ยง รวมไปถึงหัวใจ ข้อต่อ หลอดเลือด สมอง และผิวหนัง<br><br>
+
+กรดไขมัน 30 ชนิดที่สำคัญนี้ ยังช่วยในการป้องกันการแข็งตัวของหลอดเลือดแดง ซึ่งก่อให้เกิดโรคหัวใจ</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-17" href="javascript:;">อะไรเป็นสิ่งที่ทำให้ VetzPetz® มีความพิเศษเหนือกว่าผลิตภัณฑ์อื่นๆ ที่วางขายอยู่ในปัจจุบันตามท้องตลาด</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-17" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>VetzPetz® Antinol เป็นผลิตภัณฑ์อาหารเสริมชนิดเดียวในโลกที่มีส่วนผสมของไขมัน 6 ประเภท, ส่วนผสมจากทะเล 10 ชนิด
+และกรดไขมันอันเป็นประโยชน์กว่า 30 อย่าง รวมกัน ซึ่งช่วยรักษาสุขภาพหัวใจและหลอดเลือด รวมไปถึงเพิ่มความคล่องตัว
+และความยืดหยุ่นของข้อต่อในส่วนต่างๆ มีสารอาหารที่จำเป็นสำหรับการมีสุขภาพขนและผิวหนัง</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+
+  <!-- question-->
+  <div class="container-fluid marketing ">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <ul class="antinol-ul questions">
+            <li><img src="./img/liststyle.png" width="12"><a class="toggle-ans" data-target="ans-18" href="javascript:;">VetzPetz® มีกระบวนการในการสกัดน้ำมันชนิดนี้ได้อย่างไร</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="ans-18" class="container-fluid marketing questions-answer">
+    <div class="container marketing">
+      <div class="row ">
+        <div class="col-md-12">
+          <p>ก่อนการเก็บหอยแมลงภู่เพื่อนำมาสกัดน้ำมัน เราจะต้องทำการตรวจสอบหอยเหล่านี้ว่ามีสภาพที่สมบูรณ์ และอุดมไปด้วยไขมัน
+เพื่อให้ได้น้ำมันที่มีคุณภาพดีที่สุด<br><br>
+
+หลังจากนั้นหอยแมลงภู่จะส่งตรงจากมหาสมุทรแปซิฟิกมายังโรงงานทันทีเพื่อทำการแช่แข็งก่อนที่จะเริ่มต้นกระบวนการ 
+สกัดน้ำมันอันเป็นสูตรเฉพาะและได้รับการจดสิทธิบัตรรับรอง โรงงานและกระบวนการผลิตมีการควบคุมมาตรฐานสูงสุด 
+ผ่านการรับรองและตรวจสอบกระบวนการผลิตอย่างเครงครัด โดยสถาบัน MAF ซึ่งเป็นสถาบันที่ได้รับการรับรองจาก 
+องค์การอาหารและยาของสหรัฐอเมริกา และในกลุ่มสหภาพยุโรป และเอเชีย ทั้งนี้สถาบัน MAF ยังเป็นที่ยอมรับของรัฐบาล 
+ของประเทศออสเตรเลียและนิวซีแลนด์อีกด้วย</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end question -->
+  <div class="container-fluid marketing " style="height: 2rem">
+
+  </div>
+<script type="text/javascript">
+  $(function() {
+    $(document).on('click', ".toggle-ans", function() {
+      $(".questions-answer").slideUp( "slow", function() {});
+           $( "#"+$(this).attr("data-target")).slideToggle( "slow", function() {
+              
+            });
+        });
+  });
+</script>
+    <?php include ('footer.php'); ?>
