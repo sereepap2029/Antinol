65026620b5b19891c8e5742c09ab077bb4eda4a7
sereepap <sereepap2029@gmail.com>
2017-08-31 04:18:37 +0700

    finish benefit popup

--- index.php
+++ index.php
@@ -97,15 +97,15 @@ will be distracted by content</p>
         </div>
         <div class="col-md-5">
           <ul class="antinol-ul">
-            <li><img src="./img/liststyle.png" width="12">VetzPetz Antinol คืออะไร</li>
-            <li><img src="./img/liststyle.png" width="12">ประโยชน์ต่อสุขภาพ</li>
-            <li><img src="./img/liststyle.png" width="12">ทำไมจึงต้องเลือกใช้ VetzPetz</li>
-            <li><img src="./img/liststyle.png" width="12">ต้องใช้เวลานานเท่าไหร่จึงจะเริ่ม<br>ความเปลี่ยนแปลง</li>
-            <li><img src="./img/liststyle.png" width="12">อะไรเป็นสิ่งที่ทำให้ Vetz Petz<br>
+            <li data-toggle="modal" data-target="#benefits-modal-1"><img src="./img/liststyle.png" width="12">VetzPetz Antinol คืออะไร</li>
+            <li data-toggle="modal" data-target="#benefits-modal-2"><img src="./img/liststyle.png" width="12">ประโยชน์ต่อสุขภาพ</li>
+            <li data-toggle="modal" data-target="#benefits-modal-3"><img src="./img/liststyle.png" width="12">ทำไมจึงต้องเลือกใช้ VetzPetz</li>
+            <li data-toggle="modal" data-target="#benefits-modal-4"><img src="./img/liststyle.png" width="12">ต้องใช้เวลานานเท่าไหร่จึงจะเริ่ม<br>ความเปลี่ยนแปลง</li>
+            <li data-toggle="modal" data-target="#benefits-modal-5"><img src="./img/liststyle.png" width="12">อะไรเป็นสิ่งที่ทำให้ Vetz Petz<br>
 มีความพิเศษเหนือกว่าผลิตภัณฑ์<br>
 อื่นๆ
 </li>
-            <li><img src="./img/liststyle.png" width="12">สารอาหารจากอาหารสุนัข<br>
+            <li data-toggle="modal" data-target="#benefits-modal-6"><img src="./img/liststyle.png" width="12">สารอาหารจากอาหารสุนัข<br>
 ยังมีคุณค่าไม่เพียงพอ<br>
 ต่อสุนัขอีกหรือ
 </li>
@@ -168,5 +168,189 @@ will be distracted by content</p>
       
 
     </div><!-- /.container -->
+
+<!-- Modal -->
+<div class="modal fade" id="benefits-modal-1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">VetzPetz Antinol คืออะไร</h2>
+                <p class="detail-p">คือผลิตภัณฑ์ที่ได้ผ่านกระบวนการสกัดอย่างเข้มข้นจากหอยแมลงภู่เปลือก
+  สีเขียว จากนิวซีแลนด์ ซึ่งได้กลายเป็นที่รู้จักกันในทางวิทยาศาสตร์ภายใต้
+  เครื่องหมายทางการค้าในนามของสารสกัด PCSO-524
+<br><br>
+                100% ของตัวยามาจากธรรมชาติไม่ก่อให้เกิดผลข้างเคียงใดๆ เห็นผลได้
+  อย่างรวดเร็ว สามารถใช้ร่วมกับยาชนิดอื่นได้ไม่ก่อให้เกิดอาการแพ้</p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="benefits-modal-2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">ประโยชน์ต่อสุขภาพ</h2>
+                <p class="detail-p">ได้รับการแนะนำโดยสัตวแพทย์ มีผลวิจัยรับรองว่าปลอดภัยต่อสุนัขของท่าน 
+อีกทั้งยังช่วยทำให้สุขภาพโดยรวมของสุนัขของท่านดีขึ้นของพวกเขา</p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="benefits-modal-3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">ทำไมจึงต้องเลือกใช้ VetzPetz</h2>
+                <p class="detail-p"><ul>
+                  <li>เป็นสารสกัดที่ปลอดภัยจากธรรมชาติ ช่วยลดอาการบาดเจ็บ บรรเทาความ
+  เจ็บปวดตามข้อ และอาการข้อต่ออักเสบ</li>
+  <li>ช่วยลดการอักเสบแบบเฉียบพลันและเรื้อรังอีกทั้งยังช่วยส่งเสริมกระบวนการ
+  บำบัดสุนัขของคุณตามธรรมชาติ</li>
+  <li>ส่งเสริมสุขภาพที่ดีในขณะเดียวกันลดการอักเสบที่เกี่ยวกับการแพ้และ
+  ระคายเคืองที่บริเวณผิวหนัง</li>
+  <li>ช่วยทำให้สุนัขมีอายุยืนยาวขึ้น ขนสวยนุ่ม มันวาว</li>
+  <li>เป็นสารสกัดจากธรรมชาติที่ผ่านกระบวนการควบคุมการผลิตที่ได้รับมาตรฐาน
+  และคำนึงถึงความปลอดภัยของสุนัข</li>
+<li>เป็นผลิตภัณฑ์ที่ใช้ได้อย่างปลอดภัยและมีประสิทธิภาพ โดยไม่มีผลข้างเคียง
+  ใดๆ</li>
+
+                </ul></p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="benefits-modal-4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">ต้องใช้เวลานานเท่าไหร่จึงจะเริ่มความเปลี่ยนแปลง</h2>
+                <p class="detail-p">สุนัขจำนวนมากมักจะเห็นผงในระยะเวลาเพียงแค่ไม่กี่วันหลังจากเริ่มใช้ 
+VetzPetz® อย่างไรก็ตามเพื่อให้ผลที่เต็มที่ควรจะให้เวลาสักนิดรอดูผล
+หลังจากที่ใช้อย่างต่อเนื่องเป็นเวลาสัก 4-6 สัปดาห์ ในบางกรณีในระยะเริ่มแรก
+ของการบำบัด ท่านควรจะให้ VetzPetz® ในปริมาณ 2 เท่าของ ในช่วงระยะ
+สองสัปดาห์แรก ซึ่งจะเป็นการเตรียมตัวเริ่มต้นกระบวนการบำบัดได้อย่าง
+เต็มที่</p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="benefits-modal-5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">อะไรเป็นสิ่งที่ทำให้ Vetz Petz มีความพิเศษ<br>
+เหนือกว่าผลิตภัณฑ์อื่น ๆ</h2>
+                <p class="detail-p">VetzPetz® Antinol ประกอบไปด้วยส่วนผสมของไขมัน 6 ประเภท, ส่วนผสม
+จากทะเล 10 ชนิด และกรดไขมันอันเป็นประโยชน์กว่า 30 อย่าง รวมกัน ซึ่งคุณ
+ไม่สามารถหาได้จากแหล่งใดตามธรรมชาติ ในบางครั้งท่านอาจจะสับสนระหว่าง
+สารสกัด PCSO-524กับผลิตภัณฑ์สารสกัดจากเปลือกหอยแมลงภู่ ในรูปแบบ
+ผงหรือสารโอเมก้า3 อื่นๆ กล่าวสั้นๆคือ VetzPetz Antinol โดดเด่นในด้าน
+ของ การรักษาอาการอักเสบและบาดเจ็บตามร่างกายของสุนัข
+<br><br>
+นั่นคือเป้าหมายและประโยชน์อันแท้จริงของสารสกัดของเรา ผลจากการใช้
+อาหารเสริม VetzPetz เป็นประจำทุกวันนั่นก็คือ จะช่วยให้สุนัขของท่านมีอายุ
+ที่ยืนยาวขึ้น มีความสุขสดใส และแลดูกระตือรือร้น สามารถทำกิจกรรมต่างๆ
+ได้มากขึ้น</p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="benefits-modal-6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12"><h2 class="benefits-heading">Benefits of Vetz Petz:<br><span class="text-in">ประโยชน์ของ VetzPetz</span></h2></div>
+              <div class="col-md-12 detail">
+                <h3 class="detail-heading">สารอาหารจากอาหารสุนัขยังมีคุณค่าไม่เพียงพอ<br>
+ต่อสุนัขอีกหรือ ?
+</h2>
+                <p class="detail-p">โดยมากแล้วอาหารสำหรับสุนัขมักจะทำมาจากส่วนผสมที่ไม่ค่อยจะมีคุณภาพ 
+ส่วนมากมักจะผลิตจากเนื้อที่เหลือจากการบริโภคของมนุษย์หรือไม่ก็เป็นเนื้อ
+ในส่วนที่มนุษย์ไม่นิยมจะบริโภค แม้กระทั่งผลิตภัณฑ์ที่เรียกตนเองว่าจัดอยู่
+ในกลุ่ม “อาหารจากธรรมชาติ” หรือ “พรีเมี่ยม” ก็ยังคงมีข้อสงสัยเกี่ยวกับ
+คุณภาพของอาหารอยู่ดี
+<br><br>
+สุนัขของฉันกินอาหารที่ปลอดสารหรือที่เรียกว่าออร์แกนิกอยู่ มันยังไม่ดีพอ
+อีกหรือ ?
+<br><br>
+อย่าหลงกลพวกผลิตภัณฑ์ที่จำอ้างว่าเป็นพวกออร์แกนิก จากธรรมชาติ หรือ 
+บริสุทธิ์ คุณมาถูกทางแล้วล่ะที่เลือกอาหารจำพวกปลอดสารเคมี แต่อย่างไร
+ก็ตามสุนัขของท่านยังต้องการกรดไขมันซึ่งหาไม่ได้ในอาหารสุนัขทั่วไป
+</p>
+              </div>
+              <div class="col-md-12">
+                <img class="logo-img" src="./img/logo_head.png">
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+</div>
 
     <?php include ('footer.php'); ?>
